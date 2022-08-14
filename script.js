f1fcbec2d398c9cd0e04cc32532f96936e1e733f
nithin-meela <nithinmeela07@gmail.com>
2022-08-14 12:43:26 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,81 @@
+const pokeAPIBaseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+const colors = {
+  fire: '#FDDFDF',
+  grass: '#DEFDE0',
+  electric: '#FCF7DE',
+  water: '#DEF3FD',
+  ground: '#f4e7da',
+  rock: '#d5d5d4',
+  fairy: '#fceaff',
+  poison: '#98d7a5',
+  bug: '#f8d5a3',
+  dragon: '#97b3e6',
+  psychic: '#eaeda1',
+  flying: '#F5F5F5',
+  fighting: '#E6E0D4',
+  normal: '#F5F5F5',
+};
+const game = document.getElementById('game');
+let isPaused = false;
+let firstPick;
+const loadPokemon = async () => {
+  const randomIds = new Set();
+  while (randomIds.size < 8) {
+    randomIds.add(Math.ceil(Math.random() * 150)); // Generating a random number and adding that to set
+  }
+  const pokePromises = [...randomIds].map((id) => fetch(pokeAPIBaseUrl + id));
+  const response = await Promise.all(pokePromises);
+  const pokemon = await Promise.all(response.map((res) => res.json()));
+  return pokemon;
+};
+
+const displayPokemon = (pokemon) => {
+  pokemon.sort((_) => Math.random() - 0.5);
+  const pokemonHtml = pokemon
+    .map((pokemon) => {
+      const type = pokemon.types[0]?.type?.name || 'normal';
+      const color = colors[type];
+      return `
+        <div class="card" style="background-color:${color}" onclick="clickCard(event)" data-pokename="${pokemon.name}">
+            <div class="front">
+            </div>
+            <div class="back rotated" style="background-color:${color}">
+                <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}"/>
+                <h3>${pokemon.name}</h3>
+            </div>
+        </div>
+        `;
+    })
+    .join('');
+  console.log(pokemonHtml);
+  game.innerHTML = pokemonHtml;
+};
+
+const clickCard = (event) => {
+  const pokemonCard = event.currentTarget;
+  const [front, back] = getFrontAndBack(pokemonCard);
+  if (front.classList.contains('rotated') || isPaused) return;
+  isPaused = true;
+  front.classList.toggle('rotated');
+  back.classList.toggle('rotated');
+  if (!firstPick) {
+    firstPick = pokemonCard;
+    isPaused = false;
+  } else {
+    const secondPokemonName = pokemonCard.dataset.pokename;
+    const firstPokemonName = firstPick.dataset.pokename;
+    if (firstPokemonName != secondPokemonName) {
+    }
+  }
+};
+
+const getFrontAndBack = (card) => {
+  const front = card.querySelector('.front');
+  const back = card.querySelector('.back');
+  return [front, back];
+};
+const resetGame = async () => {
+  const pokemon = await loadPokemon();
+  displayPokemon([...pokemon, ...pokemon]);
+};
+resetGame();
